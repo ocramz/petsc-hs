d9502e5977523b2a172a66c6c7f82af5637a4336
ocramz <zocca.marco@gmail.com>
2015-10-06 15:43:25 +0200

    removed Numerical.PETSc.Raw for now

--- src/Numerical/PETSc/Main.hs
+++ src/Numerical/PETSc/Main.hs
@@ -1,27 +1,8 @@
 module Numerical.PETSc.Main where
 
-import Numerical.PETSc.Internal
-import Control.Monad (unless, when)
--- import Numerical.PETSc.Types
+-- import Numerical.PETSc.Raw.PutGet
 
 
 main :: IO ()
 main = 
   return ()
-
-withPetsc f = do
-  ei <- petscInit'
-  f
-  putStrLn $ "so far so good" ++ show ei
-  petscFin'
-
--- withPtr_ f = do
---   (x, ()) <- withPtr f
---   return x
-
--- withPtr_ :: (Storable a) => (Ptr a -> IO ()) -> IO a
--- withPtr f = do
---   alloca $ \ptr -> do
---     x <- f ptr
---     y <- peek ptr
---     return (y, x)
