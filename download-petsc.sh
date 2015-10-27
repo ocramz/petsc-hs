2783d396c692cfcbebe57303c04d522b4c1b6641
ocramz <zocca.marco@gmail.com>
2015-10-27 09:10:53 +0100

    separate download/install petsc in travis file

---
+++ download-petsc.sh
@@ -0,0 +1,9 @@
+/usr/bin/env sh
+
+mkdir petsc
+cd petsc
+
+wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.6.2.tar.gz
+tar xzvf petsc-lite-3.6.2.tar.gz
+
+cd ..
--- install-petsc.sh
+++ install-petsc.sh
@@ -1,11 +1,8 @@
 /usr/bin/env sh
-mkdir temp
-cd temp
 
-wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.6.2.tar.gz
-tar xzvf petsc-lite-3.6.2.tar.gz
+cd petsc
 cd petsc-3.6.2
 
 ./configure --with-cc=gcc --with-cxx=g++ --with-fc=gfortran --download-fblaslapack --download-mpich
 
-make all test
+# make all test
