c0b4925ea896a93f3b6be6cae12faf507147dee7
ocramz <zocca.marco@gmail.com>
2015-10-27 17:07:42 +0100

    simplified Raw/PutGet.hs

--- src/Numerical/PETSc/Raw/PutGet.hs
+++ src/Numerical/PETSc/Raw/PutGet.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE TypeFamilies, MultiParamTypeClasses, RankNTypes#-}
-{-# LANGUAGE CPP #-}
-
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Numerical.PETSc.Raw.PutGet
@@ -13,38 +10,19 @@
 --   scope of `bracket`s
 --
 -----------------------------------------------------------------------------
-module Numerical.PETSc.Raw.PutGet
-       (
-         module Numerical.PETSc.Raw.PutGet.IS,
-         module Numerical.PETSc.Raw.PutGet.Vec,
-         module Numerical.PETSc.Raw.PutGet.Mat,
-         module Numerical.PETSc.Raw.PutGet.DM,
-         module Numerical.PETSc.Raw.PutGet.KSP,
-         module Numerical.PETSc.Raw.PutGet.SNES,
-         module Numerical.PETSc.Raw.PutGet.TS,
-         module Numerical.PETSc.Raw.PutGet.TAO,
-         module Numerical.PETSc.Raw.PutGet.Viewer,
-         module Numerical.PETSc.Raw.PutGet.PetscMisc
-       )
+module Numerical.PETSc.Raw.PutGet ( module X )
        where
 
--- import Numerical.PETSc.Raw.InlineC
--- import Numerical.PETSc.Raw.Types
--- import Numerical.PETSc.Raw.Exception
--- import Numerical.PETSc.Raw.Utils
-
--- import Numerical.PETSc.Raw.Internal
-
-import Numerical.PETSc.Raw.PutGet.IS
-import Numerical.PETSc.Raw.PutGet.Vec
-import Numerical.PETSc.Raw.PutGet.Mat
-import Numerical.PETSc.Raw.PutGet.DM
-import Numerical.PETSc.Raw.PutGet.KSP
-import Numerical.PETSc.Raw.PutGet.SNES
-import Numerical.PETSc.Raw.PutGet.TS
-import Numerical.PETSc.Raw.PutGet.TAO
-import Numerical.PETSc.Raw.PutGet.Viewer
-import Numerical.PETSc.Raw.PutGet.PetscMisc
+import Numerical.PETSc.Raw.PutGet.IS as X 
+import Numerical.PETSc.Raw.PutGet.Vec as X
+import Numerical.PETSc.Raw.PutGet.Mat as X
+import Numerical.PETSc.Raw.PutGet.DM as X
+import Numerical.PETSc.Raw.PutGet.KSP as X
+import Numerical.PETSc.Raw.PutGet.SNES as X
+import Numerical.PETSc.Raw.PutGet.TS as X
+import Numerical.PETSc.Raw.PutGet.TAO as X
+import Numerical.PETSc.Raw.PutGet.Viewer as X
+import Numerical.PETSc.Raw.PutGet.PetscMisc as X
 
 
 
