d70740de9272d7227a53470b3e5f45c3a504b68e
ocramz <zocca.marco@gmail.com>
2015-10-31 18:38:52 +0100

    moving Storable adapters ni Storable.Common

---
+++ src/Numerical/PETSc/Raw/Storable/Common.hs
@@ -0,0 +1,47 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Numerical.PETSc.Raw.Storable.Common
+-- Copyright   :  (c) Marco Zocca 2015
+-- License     :  LGPL3
+-- Maintainer  :  Marco Zocca
+-- Stability   :  experimental
+--
+-- | common adapters for Storable
+--
+-----------------------------------------------------------------------------
+module Numerical.PETSc.Raw.Storable.Common where
+
+import Data.Functor
+import Control.Applicative
+import Control.Monad
+
+import Foreign.C.Types
+
+
+liftF1_ ::
+  Monad m =>
+  (r -> m a) ->
+  (r -> b -> m c) ->
+  r ->
+  (a -> b) ->
+  m c
+liftF1_ get set x f = do
+  v <- get x
+  set x (f v)
+
+-- -- e.g. 
+-- liftVectorF1_ :: Vec -> (V.Vector PetscScalar_ -> V.Vector PetscScalar_) -> IO ()
+-- liftVectorF1_ = liftF1_ vecGetVector vecRestoreVector
+
+liftF2 ::
+  (Functor m, Monad m) =>
+  (a -> b -> c) ->
+  a ->
+  (d -> m b) ->
+  (d -> c -> m e) ->
+  d ->
+  m CInt
+liftF2 f a getter setter b = do
+  y <- f a <$> getter b
+  setter b y
+  return (0 :: CInt)
