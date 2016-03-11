a494b27f1724571623bf14351c9f1dc9c7bbf798
ocramz <zocca.marco@gmail.com>
2016-03-11 11:28:31 +0100

    a REPL for petsc-hs ?

---
+++ src/Numerical/PETSc/Repl.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Numerical.PETSc.Repl
+-- Copyright   :  (c) Marco Zocca 2015
+-- License     :  LGPL3
+-- Maintainer  :  zocca . marco . gmail . com
+-- Stability   :  experimental
+--
+-- | Read-Eval-Print Loop
+--
+-----------------------------------------------------------------------------
+module Numerical.PETSc.Repl where
+
+import Numerical.PETSc.Internal.PutGet
+import Numerical.PETSc.Internal.Types
+
+
+-- | see e.g. https://hackage.haskell.org/package/repline-0.1.5.0/docs/src/System-Console-Repline.html
