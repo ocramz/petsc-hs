0ac07446d7f6e7c7cd320ebbc7c5c8b8b543104f
ocramz <zocca.marco@gmail.com>
2016-04-12 23:58:24 +0200

    renamed IO to Display

--- src/Numerical/PETSc/Internal/Display.hs
+++ src/Numerical/PETSc/Internal/Display.hs
@@ -6,10 +6,10 @@
 -- Maintainer  :  zocca . marco . gmail . com
 -- Stability   :  experimental
 --
--- | misc. I/O operations
+-- | misc. display operations
 --
 -----------------------------------------------------------------------------
-module Numerical.PETSc.Internal.IO where
+module Numerical.PETSc.Internal.Display where
 
 import           Data.List
 
@@ -48,6 +48,8 @@ showV l = showVn l 20
 
 -- padSparse (i, x) v = if i==v then show x else "."
 
+showSparse :: Show a => [(Int, a)] -> Int -> String
+showSparse x n = unwords $ padSparse x [0..n-1]
 
 -- | padSparse : display sparse (index, data) array with dots instead of the 0 entries
 padSparse [] _ = []
@@ -56,8 +58,9 @@ padSparse xx@((i,x):xs) (v:vs)
   | i==v = show x : padSparse xs vs
   | otherwise = "." : padSparse xx vs
 
-asdf = unwords $ padSparse [(1, "x"), (3, "w")] [1..10]
 
+-- -- examples:
+asdf = unwords $ padSparse [(1, "x"), (3, "w")] [1..10]
 fdsa = unwords $ padSparse [(1, 1), (3, 3), (4, 4), (7, 9)] [1..10]
 
 
