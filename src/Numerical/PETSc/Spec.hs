f45aff75a8bf86092cfc7a08ec173cf8e258c41f
ocramz <zocca.marco@gmail.com>
2016-06-02 18:36:44 +0200

    mirror mirror, y SLEPc no work locally?

--- src/Numerical/PETSc/Spec.hs
+++ src/Numerical/PETSc/Spec.hs
@@ -34,13 +34,13 @@ import Test.Hspec
 
 specs :: IO ()
 specs = 
-  -- withPetsc0 $
-  withSlepc0 $ hspec $ do
-  --  t_vecDot_r2_1
-  --  t_linSys_r3_1
-  -- --  -- 
-  -- -- withSlepc0 $ hspec $ do
-   t_eigen_r3_1
+  withPetsc0 $
+   withSlepc0 $ hspec $ do
+   --  t_vecDot_r2_1
+   --  t_linSys_r3_1
+   -- --  -- 
+   -- -- withSlepc0 $ hspec $ do
+    t_eigen_r3_1
 
 
 
