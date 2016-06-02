30e19765e4a3541bfe775d37e997cea11dd813d5
ocramz <zocca.marco@gmail.com>
2016-06-02 23:28:21 +0200

    fixing Specs

--- src/Numerical/PETSc/Spec.hs
+++ src/Numerical/PETSc/Spec.hs
@@ -108,7 +108,7 @@ t_eigen_r3_1 = describe "t_eigen_r3_1" $
 
 -- 3x3 asymmetric matrix :
 -- elements
-ixd3x3 = listToCSR 3 3 [1,2,0,0,3,4,5,6,7]
+ixd3x3 = listToCSR 3 3 [1,2,0,0,3,4,5,68234,7]
 -- nonzero pattern
 nz3x3 = VarNZPR (dnnz, onnz) where
    dnnz = V.convert $ V.fromList [1,1,1]
