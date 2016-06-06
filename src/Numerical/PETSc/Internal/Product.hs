1ba820835b0dff9a30b8f9f101cf4b3414710dc7
ocramz <zocca.marco@gmail.com>
2016-06-06 18:28:06 +0200

    adding Product interface

--- src/Numerical/PETSc/Internal/Product.hs
+++ src/Numerical/PETSc/Internal/Product.hs
@@ -11,10 +11,28 @@
 -----------------------------------------------------------------------------
 module Numerical.PETSc.Internal.Product where
 
+import Numerical.PETSc.Internal.PutGet.Mat
+
+import qualified Data.Foldable as Fold
+
+import qualified Data.Vector as V
 
 
-sizeCompat2 a b sz sz2
-  | sz a == sz2 b = True
-  | otherwise = False
+-- sizeCompat2 a b sz sz2
+--   | sz a == sz2 b = True
+--   | otherwise = False
 
-sizeCompatN sz sz2 = all (\x y -> sz x == sz2 y) -- (head t)
+-- | are all inner dimensions compatible for matrix product?
+
+sizeCompatN :: (Eq a) => (t -> a) -> (t -> a) -> [t] -> Bool
+sizeCompatN sz sz2 tt =
+  all (\(x, y) -> sz x == sz2 y) $ zip tt (tail tt)
+
+sizeCompatNV :: (Eq a) => (t -> a) -> (t -> a) -> V.Vector t -> Bool
+sizeCompatNV sz sz2 tt =
+  V.all (\(x, y) -> sz x == sz2 y) $ V.zip tt (V.tail tt)
+
+
+sizeCompatMat = sizeCompatNV fm1 fm2 where
+  fm1 = matRows . getMatrixInfoBase
+  fm2 = matCols . getMatrixInfoBase
