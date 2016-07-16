115f23d9397f807bf3497546016bfeeab6d15d75
ocramz <zocca.marco@gmail.com>
2016-07-16 18:27:53 +0200

    asdf

--- c2hs-build.sh
+++ c2hs-build.sh
@@ -19,9 +19,14 @@ SLEPC_LIB="$SLEPC_DIR"/"$SLEPC_ARCH"/lib/
 C2HS_GEN_FILE=TypesC2HsGen 
 
 # # generate c2hs
+printf "\n=== Generate C2HS bindings:"
 stack exec runhaskell ${C2HS_DIR}/GenerateC2Hs.hs > ${C2HS_DIR}/${C2HS_GEN_FILE}.chs
 
+printf "\n=== Contents of %s:\n" ${C2HS_DIR}
+ls -lsA ${C2HS_DIR}
+ 
 # # interpret c2hs -> render Haskell bindings
+printf "\n=== Run c2hs -> render Haskell FFI bindings:\n"
 stack exec c2hs --  -C -I${PETSC_INCLUDE1} -C -I${PETSC_INCLUDE2} -C -I${SLEPC_INCLUDE1} -C -I${SLEPC_INCLUDE2} -o ${C2HS_DIR}/${C2HS_GEN_FILE}.hs ${C2HS_DIR}/${C2HS_GEN_FILE}.chs
 
 # stack exec c2hs --  -C -I${PETSC_INCLUDE2} -C -I${PETSC_INCLUDE1}  -C -I${SLEPC_INCLUDE1} -o ${C2HS_DIR}/${C2HS_GEN_FILE}.hs ${C2HS_DIR}/${C2HS_GEN_FILE}.chs
