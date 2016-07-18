45ed99091053cdd161bbb77b04a65838c445c29a
ocramz <zocca.marco@gmail.com>
2016-07-18 12:18:41 +0200

    c2hs gen trace info

--- c2hs-build.sh
+++ c2hs-build.sh
@@ -27,7 +27,7 @@ ls -lsA ${C2HS_DIR}
  
 # # interpret c2hs -> render Haskell bindings
 printf "\n=== Run c2hs -> render Haskell FFI bindings:\n"
-printf "\n%s " stack exec c2hs --  -C -I${PETSC_INCLUDE1} -C -I${PETSC_INCLUDE2} -C -I${SLEPC_INCLUDE1} -C -I${SLEPC_INCLUDE2} -o ${C2HS_DIR}/${C2HS_GEN_FILE}.hs ${C2HS_DIR}/${C2HS_GEN_FILE}.chs
+printf "\n%s\n" "stack exec c2hs --  -C -I${PETSC_INCLUDE1} -C -I${PETSC_INCLUDE2} -C -I${SLEPC_INCLUDE1} -C -I${SLEPC_INCLUDE2} -o ${C2HS_DIR}/${C2HS_GEN_FILE}.hs ${C2HS_DIR}/${C2HS_GEN_FILE}.chs"
 stack exec c2hs --  -C -I${PETSC_INCLUDE1} -C -I${PETSC_INCLUDE2} -C -I${SLEPC_INCLUDE1} -C -I${SLEPC_INCLUDE2} -o ${C2HS_DIR}/${C2HS_GEN_FILE}.hs ${C2HS_DIR}/${C2HS_GEN_FILE}.chs
 
 # stack exec c2hs --  -C -I${PETSC_INCLUDE2} -C -I${PETSC_INCLUDE1}  -C -I${SLEPC_INCLUDE1} -o ${C2HS_DIR}/${C2HS_GEN_FILE}.hs ${C2HS_DIR}/${C2HS_GEN_FILE}.chs
